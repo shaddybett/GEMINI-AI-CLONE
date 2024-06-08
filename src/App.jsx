66e37f211e1691105cc79af8000551bde86f6942
mwendwamark <mwendwamark310@gmail.com>
2024-06-08 12:47:17 +0300

    create the ui for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/sidebar/sidebar";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <>
     <Sidebar/>
+    <Main />
     </>
   );
 }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Main.css";
+import userpic from "../../assets/userpic1.jpg";
+import { FaRegCompass } from "react-icons/fa6";
+import { GoLightBulb } from "react-icons/go";
+import { FaRegMessage } from "react-icons/fa6";
+import { FaCode } from "react-icons/fa6";
+import { GrGallery } from "react-icons/gr";
+import { FaMicrophone } from "react-icons/fa";
+import { FiSend } from "react-icons/fi";
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+          <p>Gemini Clone</p>
+          <img src={userpic} alt="" />
+        </div>
+
+        <div className="main-container">
+          <div className="greet">
+            <p>
+              <span>Hello, Mark</span>
+            </p>
+            <p>How can I help you today?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Suggest beautiful places to see on an upcomung road trip</p>
+              <FaRegCompass className="card-icon" />{" "}
+            </div>
+
+            <div className="card">
+              <p>Briefly summarize this concept: urban planning</p>
+              <GoLightBulb className="card-icon" />{" "}
+            </div>
+
+            <div className="card">
+              <p>Brainstorm team bonding activities for our work retreat</p>
+              <FaRegMessage className="card-icon" />{" "}
+            </div>
+
+            <div className="card">
+              <p>Improve the readability of the following code</p>
+              <FaCode className="card-icon" />{" "}
+            </div>
+          </div>
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder="Enter a prompt here" />
+              <div>
+                <GrGallery className="search-box-icon"/>
+                <FaMicrophone className="search-box-icon"/>
+                <FiSend className="search-box-icon"/>
+              </div>
+            </div>
+            <p className="bottom-info">Gemini clone may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini clone Apps</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
