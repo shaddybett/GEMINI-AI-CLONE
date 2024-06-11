5dadb70ece3c8f4c1a1b83eb20bf589a07200a95
mwendwamark <mwendwamark310@gmail.com>
2024-06-11 13:12:50 +0300

    remove the asterix from being displyed

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,13 +11,26 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  // const delay_param = (index, nextWord) => {};
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input)
+    setRecentPrompt(input);
     const response = await runChat(input); //response to be stored in this variable
-    setResultData(response);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newResponse.split("*").join("</br>");
+    setResultData(newResponse2);
     setLoading(false);
     setInput("");
   };
